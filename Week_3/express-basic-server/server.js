6ca55754932f8644a7e219519575133f41ffd839
ErmiyasHailemichael <91638042+ErmiyasHailemichael@users.noreply.github.com>
2026-01-29 21:38:01 -0800

    week 3 - express basic server step 4

--- Week_3/express-basic-server/server.js
+++ Week_3/express-basic-server/server.js
@@ -21,6 +21,30 @@ app.get('/about', (req, res) => {
   res.send('About page');
 });
 
+// Route 3: Conditional routing - /foo
+// First handler - randomly decides what to do
+app.get('/foo', (req, res, next) => {
+  // Generate random number between 0 and 1
+  const random = Math.random();
+  
+  console.log(`Random value: ${random}`); 
+  
+  if (random > 0.5) {
+    // 50% chance - send response and stop
+    res.send('sometimes this');
+  } else {
+    // 50% chance - pass control to next handler
+    console.log('Calling next()...');
+    next();
+  }
+});
+
+// Route 4: Second handler for /foo
+// Only runs if first handler calls next()
+app.get('/foo', (req, res) => {
+  res.send('and sometimes that');
+});
+
 // ========================================
 // START SERVER
 // ========================================
