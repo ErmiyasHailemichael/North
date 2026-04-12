ccba2632f76e57f67f86b9af9d15b1274c246a19
ErmiyasHailemichael <91638042+ErmiyasHailemichael@users.noreply.github.com>
2026-04-12 15:52:01 -0700

    Reorganize: Move all content into Intermediate_Dev_I folder

--- week_6/remix-blog-app/app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, route, index } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("blog/:slug", "routes/blog.$slug.tsx"),
-  route("blog/category/:tag", "routes/blog.category.$tag.tsx"),
-] satisfies RouteConfig;
--- week_8/feature-toggle-demo/src/FeatureToggle.jsx
+++
@@ -1,9 +0,0 @@
-function FeatureToggle(props) {
-  if (props.isEnabled) {
-    return <p>{props.featureName}</p>;
-  }
-
-  return <p>Feature {props.featureName} is disabled</p>;
-}
-
-export default FeatureToggle;
